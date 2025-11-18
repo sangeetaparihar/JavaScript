8e11bba902c5fbd9b70428c5fff34fdc548fa77f
sangeetaparihar <sangeetaparihar25@navgurukul.org>
2025-11-18 12:24:20 +0530

    Create Q1.js

---
+++ Q1.js
@@ -0,0 +1,33 @@
+//Problem Statement:
+//rite a program that takes an integer as input and prints whether it is "Positive", "Negative", or "Zero".
+
+//Constraints:
+//Do not use any inbuilt functions, methods, or external libraries.
+
+//Sample Input 1:
+//15
+
+//Sample Output 1:
+//Positive
+
+//Sample Input 2:
+//-3
+
+//Sample Output 2:
+//Negative
+
+//Sample Input 3:
+//0
+
+//Sample Output 3:
+//Zero
+
+
+
+
+
+
+
+
+
+
